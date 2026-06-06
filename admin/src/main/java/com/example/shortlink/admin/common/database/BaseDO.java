1ab43730fdab5eb122e9649227d4de780d0f3777
伪装 <826405882@qq.com>
2026-06-06 17:53:41 +0800

    optimize：优化用户持久层实体基础属性

--- admin/src/main/java/com/example/shortlink/admin/common/database/BaseDO.java
+++ admin/src/main/java/com/example/shortlink/admin/common/database/BaseDO.java
@@ -2,12 +2,14 @@ package com.example.shortlink.admin.common.database;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
 
 import java.util.Date;
 
 /**
  * 数据库持久层对象基础数据
  */
+@Data
 public class BaseDO {
 
     @TableField(fill = FieldFill.INSERT)
--- admin/src/main/java/com/example/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/example/shortlink/admin/dao/entity/UserDO.java
@@ -1,13 +1,13 @@
 package com.example.shortlink.admin.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
+import com.example.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
 
-import java.util.Date;
 
 @TableName("t_user")
 @Data
-public class UserDO {
+public class UserDO extends BaseDO {
     @TableId(type = IdType.ASSIGN_ID)
     /**
      * ID
@@ -44,21 +44,5 @@ public class UserDO {
      */
     private Long deletionTime;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
 
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
 }
