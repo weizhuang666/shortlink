1571628fe5ad5c59a9e710cb61f850dbe9aa3f26
伪装 <826405882@qq.com>
2026-06-07 21:05:20 +0800

    feature：短链接分页接口新增创建时间以及时间序列化

--- project/src/main/java/com/example/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/com/example/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -1,6 +1,7 @@
 package com.example.shortlink.project.dto.resp;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -46,7 +47,11 @@ public class ShortLinkPageRespDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 
     /**
      * 描述
