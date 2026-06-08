cc3f9f32017e20503301bcc3b2b76d2b95986886
伪装 <826405882@qq.com>
2026-06-08 16:44:57 +0800

    featrue：前端传递有效时间序列化

--- admin/src/main/java/com/example/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
+++ admin/src/main/java/com/example/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package com.example.shortlink.admin.remote.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -34,6 +35,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
