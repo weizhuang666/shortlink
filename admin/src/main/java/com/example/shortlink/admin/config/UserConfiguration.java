376a162c1a51d8cf0bf6b808c0e24185c519a745
伪装 <826405882@qq.com>
2026-06-25 20:41:12 +0800

    feature：短链接后管通过风控保障系统稳定性

--- admin/src/main/java/com/example/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/example/shortlink/admin/config/UserConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.shortlink.admin.config;
 
+import com.example.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
 import com.example.shortlink.admin.common.biz.user.UserTransmitFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +24,21 @@ public class UserConfiguration {
         registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
         registration.addUrlPatterns("/*");
         registration.setOrder(0);
+        return registration;
+    }
+
+    /**
+     * 用户操作流量风控过滤器
+     */
+    @Bean
+    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+            StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(10);
         return registration;
     }
 }
